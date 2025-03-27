34508d79286872bb9e3980c954438e8b152551df
Lamsaaf Yassine <lamsaafyassine20@gmail.com>
2025-03-27 19:21:57 +0000

    first commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -24,7 +24,7 @@ export const Home = () => {
 
             <div className="flex flex-col flex-wrap md:flex-row justify-center md:justify-center gap-4">
               <a
-                href="LamsaafYassine-portfolio/cv.pdf"
+                href="https://drive.google.com/file/d/1CAui1WQeat_9MTY764_btg5ZkdMmbhYG/view?usp=sharing"
                 className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-500/10 "
               >
                 Download CV
