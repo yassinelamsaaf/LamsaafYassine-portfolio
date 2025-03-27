b6c26d16e5b81f9b58a80d1f583a5c84d24c0c6c
Lamsaaf Yassine <lamsaafyassine20@gmail.com>
2025-03-27 18:38:01 +0000

    first commit

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -30,7 +30,7 @@ export const Home = () => {
                 Download CV
               </a>
               <a
-                href=""
+                href="#projects"
                 className="bg-blue-500 text-white py-3 px-10 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)] "
               >
                 View Projects
